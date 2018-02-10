d287bfcb6e6a21af9c666b21d6ae8bd8a9682c18
Justus <actionjackson0@gmail.com>
2018-02-10 14:56:06 -0500

    some new stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import face from './naruto.jpg';
 
 class App extends Component {
+
+  handleClick() {
+    alert('SHADOW CLONE JUTSU')
+  }
   render() {
-    return (
+    const now = new Date();
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h2 className="App-intro">
-          Hello World.
-        </h2>
+        <Biography />
+        <br />
+        <center><Grocery /></center>
+        <br />
+        <right><Tasks /></right>
+        <p>The date is: {now.toTimeString()}</p>
+        <br />
+        <button onClick={this.handleClick.bind(this)}>Click here</button>
       </div>
     );
+  }
+}
+
+class Biography extends Component {
+  render() {
+    return (
+      <div>
+        <img src={face} style={{ float: 'left', paddingRight: 50 }} alt="meme" />
+        <h1>Naruto Uzumaki</h1>
+        <h2>Occupation: Ninja</h2>
+        <p>Believe it!</p>
+        <hr style={{clear:'left'}}/>
+      </div>
+    );
+  }
+}
+
+class Grocery extends Component {
+  render() {
+    const groceries = ['ramen','rasengan','sasuke'];
+    return (
+      <div>
+        <h2>Grocery List</h2>
+        <div style={{border: '3px solid black', width: 300}}>
+          <ul style={{listStyle: 'none', padding: 0}}>
+            {groceries.map(topic => <li>{topic}</li>)} 
+          </ul>
+        </div>
+        <br /><hr />
+      </div>
+    )
+  }
+}
+
+class Tasks extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      items: [] 
+    };
+  }
+
+  handleClick2() {
+    const items = this.state.items;
+    items.push(this.textbox.value);
+    this.setState({items: items});
+  }
+
+  handleClick3(item) {
+    const items = this.state.items;
+    var index = items.indexOf(item);
+    if (index > -1) {
+      items.splice(index,1);
+    }
+    this.setState({items: items})
+
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={textbox => this.textbox = textbox} />
+        <button onClick={this.handleClick2.bind(this)}>Add Item</button>
+        <p>To-Do List</p>
+        <ol>
+          {this.state.items.map(item => <li>{item}{' '}<input type="button" onClick={this.handleClick3.bind(this, item)} /></li>)}
+        </ol>
+        <br /><hr />
+      </div>
+    )
   }
 }
 
